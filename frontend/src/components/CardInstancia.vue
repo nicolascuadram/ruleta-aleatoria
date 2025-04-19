e67a6a0cb76abf82297fd34741f0dc8ac2fcd579
Nicolás_Cuadra <nikocuadram@gmail.com>
2025-04-19 02:27:55 -0400

    Lista de instancias

---
+++ frontend/src/components/CardInstancia.vue
@@ -0,0 +1,38 @@
+<script setup>
+
+defineProps({
+	url: {
+		type: String,
+		required: true,
+	},
+	titulo: {
+		type: String,
+		required: true,
+	},
+	semestre: {
+		type: String,
+		required: true,
+	},
+	profesor: {
+		type: String,
+		required: true,
+	},
+});
+</script>
+<template>
+	<a class="flex flex-col justify-between items-center w-[240px] h-[240px] p-4 border border-zinc-700
+        rounded-md shadow-md transition duration-300 cursor-pointer bg-zinc-950 
+        hover:bg-gradient-to-br from-zinc-950 to-zinc-800" :href="`${url}`"
+	>
+		<header class="flex flex-col items-start text-start w-full">
+			<h1 class="text-lg font-semibold">{{ titulo }}</h1>
+			<h2 class="text-sm font-medium text-zinc-300">{{ semestre }}</h2>
+		</header>
+		<aside class="flex justify-start items-center gap-2 text-center w-full">
+			<span class="flex justify-center items-center gap-2 px-2 py-1 bg-zinc-50 text-zinc-900 font-medium rounded-md shadow-md">
+				<img src="/src/assets/circle-user-round.svg" alt="Profesor" class="w-5 h-5" />
+				<p class="text-xs font-medium">{{ profesor }}</p>
+			</span>
+		</aside>
+	</a>
+</template>
---
+++ frontend/src/components/ListaInstancias.vue
@@ -0,0 +1,98 @@
+<script setup>
+import CardInstancia from './CardInstancia.vue';
+import { ref, onMounted } from 'vue';
+
+const instancias = ref([]);
+
+// Simulación de datos para instancias
+instancias.value = [
+    {
+        id: 1,
+        semestre: '2025-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 2,
+        semestre: '2025-2',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 3,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 4,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 5,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 6,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 7,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 8,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 9,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+    {
+        id: 10,
+        semestre: '2026-1',
+        profesor: 'Luis Silvestre',
+    },
+];
+
+/* const getInstancias = async (id) => {
+    try {
+        const response = await fetch("http://localhost:3000/api/instancias", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!response.ok) {
+            console.error("Status:", response.status);
+            throw new Error(
+                "Error en la respuesta del servidor: " + response.statusText
+            );
+        }
+        const data = await response.json();
+        instancias.value = data;
+    } catch (err) {
+        console.error("Error fetching instancias:", err);
+    }
+};
+
+onMounted(() => {
+    getInstancias();
+}); */
+</script>
+
+<template>
+    <section class="flex flex-col items-center justify-center w-full h-[530px] p-4">
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 overflow-y-scroll">
+            <CardInstancia v-for="instancia in instancias" :key="instancia.id" 
+                :url="`/instancia/${instancia.id}`"
+                titulo="Gestión de Proyectos Tecnológicos" 
+                :semestre="instancia.semestre"
+                :profesor="instancia.profesor"
+            />
+        </div>
+    </section>
+</template>
