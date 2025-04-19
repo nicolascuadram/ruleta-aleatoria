2b7a15b3a82093f9bcbd307492b0b5b717640608
Nicolás_Cuadra <nikocuadram@gmail.com>
2025-04-18 23:43:04 -0400

    Inicialización del Frontend y primeras vistas

---
+++ frontend/src/components/Login.vue
@@ -0,0 +1,7 @@
+<template>
+    <section class="flex justify-center items-center w-full h-full">
+        <button
+            class="bg-zinc-50 text-zinc-900 font-medium py-2 px-4 rounded-md hover:bg-zinc-300 transition duration-300 cursor-pointer shadow-md"
+            onclick="window.location.href = '/instancias'">Ingresar</button>
+    </section>
+</template>
