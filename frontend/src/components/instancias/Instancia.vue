a01f2757b8457c337400440b19ba089852341f06
Nicolás_Cuadra <nikocuadram@gmail.com>
2025-05-31 00:24:22 -0400

    Avances en ejecución: Enviar contenido para la ruleta desde Ejecucion a Instancia

--- frontend/src/components/instancias/Instancia.vue
+++ frontend/src/components/instancias/Instancia.vue
@@ -1,9 +1,11 @@
 <script setup>
+import { ref } from 'vue';
 import Equipos from "../equipos/Equipos.vue";
 import CrearEquipos from "../equipos/CrearEquipos.vue";
 import Ruleta from "../ruleta/Ruleta2Aleatoria.vue";
 import Historial from "../historial/Historial.vue";
 import SubirIncidencias from "../incidencias/SubirIncidencias.vue";
+import Ejecucion from '../Ejecucion.vue';
 
 const props = defineProps({
     id: {
@@ -11,6 +13,14 @@ const props = defineProps({
         required: true
     }
 });
+
+const roulette_content = ref([]);
+
+const updateRouletteContent = (content) => {
+    roulette_content.value = content;
+	console.log(`Contenido para la ruleta: ${roulette_content.value}`);
+};
+
 </script>
 
 <template>
@@ -23,7 +33,8 @@ const props = defineProps({
 		<!-- Lado Central -->
 		<div class="basis-2/4 grow h-full">
 			<!-- Ruleta Aleatoria -->
-			<Ruleta :id="id" />
+			<!-- <Ruleta :id="id" /> -->
+			<Ejecucion @update:roulette_content="updateRouletteContent" />
 		</div>
 		<!-- Lado Derecho -->
 		<div class="basis-1/4 grow h-full">
