b2047cd069ffaa8fa5dd6d72fa916fe06453ba40
CristianF3 <crfuentes19@alumnos.utalca.cl>
2025-06-15 21:44:35 -0400

    solucion nombre atributo queries

--- backend/routes/dbroutes.js
+++ backend/routes/dbroutes.js
@@ -66,16 +66,6 @@ router.get('/registros', async(req, res) => {
         res.status(500).json({error: "Error al obtener los registros"});
    }
 })
-router.get('/registros/:id', async(req, res) => {
-   try {
-       const { id } = req.params;
-       const result = await pool.query(getQueries.GetRegistrosByInstancia, [id]);
-       res.json(result.rows);
-   } catch (error) {
-        res.status(500).json({error: "Error al obtener los registros"});
-   }
-})
-
 
 
 //Obtener los grupos asociados a una instancia, mediante el id de la instancia
